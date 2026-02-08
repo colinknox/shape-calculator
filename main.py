daa4a7db208056ef8fbafd5c9bc7b1f6a170e0db
colinknox <colinwknox@gmail.com>
2026-02-08 15:36:07 -0500

    created Circle class

--- main.py
+++ main.py
@@ -9,10 +9,20 @@ class Rectangle:
     def get_name(self):
         return "Rectangle"
         
+class Circle:
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def get_area(self):
+        return 3.14159 * (self.__radius * self.__radius)
+    
+    def get_name(self):
+        return "Circle"
+
 
 
 test_rectangle = Rectangle(5, 3)
+test_circle = Circle(12)
 
-print(f"DEBUG: Area = {test_rectangle.get_area()}")
-print(f"DEBUG: Name = {test_rectangle.get_name()}")
-print(f"DEBUG: Width and height = {test_rectangle.get_width_and_height()}")
+# print(f"DEBUG: Get area = {test_circle.get_area()}")
+print(f"DEBUG: Get name = {test_circle.get_name()}")
